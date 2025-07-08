f2cfa560865eeb56a2a20f7fd301d790d99d1b0f
Enoch <jesulanaenoch@gmail.com>
2025-07-08 12:46:02 +0100

    feat: update Footer component with new structure and content
    - Refactored the Footer component to include a link for the logo and reorganized sections for better clarity.
    - Replaced the `Otherpages` mapping with `Footercontent` for improved content management.
    - Added an "Account" section to replace the previous "Social Media" section, utilizing `AccountItem` for account links.
    - Introduced a new "Follow Us" section for social media links, enhancing user engagement.

--- app/components/shared/Footer.tsx
+++ app/components/shared/Footer.tsx
@@ -2,7 +2,7 @@ import { sectionPadding } from "@/app/styles/styles";
 import images from "@/public/images";
 import Link from "next/link";
 import Image from "next/image";
-import { Otherpages, Contact, Socials } from "@/app/utils";
+import { Otherpages, Contact, Socials, Footercontent, AccountItem } from "@/app/utils";
 import MobileFooter from "./MobileFooter";
 
 export default function Footer() {
@@ -12,33 +12,40 @@ export default function Footer() {
             <div className={`${sectionPadding} `}>
                 <div className="flex flex-col md:flex-row md:justify-between gap-4 py-4">
                     <div className="flex flex-col gap-4 pr-4">
-                        <Image src={images.logo} alt="Modern Fashion & Family" width={100} height={100} className="invert"/>
+                        <Link href="/">
+                            <Image src={images.logo} alt="Modern Fashion & Family" width={100} height={100} className="invert"/>
+                        </Link>
                         <p className="text-white w-full md:w-[350px]">
                         Bringing style, comfort, and tech together for men, women, and kids. From timeless fashion pieces to smart gadgets, we’re your one-stop destination for the whole family
                         </p>    
                     </div>
+                    {/* Other Pages  */}
                     <div className="flex flex-col gap-4 pr-4">
                         <h2 className="text-white  font-bold">Other Pages</h2>
                         <ul className="flex flex-col gap-4 text-sm">
-                            {Otherpages.map((page, index) => (
+                            {Footercontent.map((page, index) => (
                                 <li key={index}>
                                     <Link href={page.link} className="text-white hover:text-[#FFD700] transition-all duration-300">{page.name}</Link>
                                 </li>
                             ))}
                         </ul>
                     </div>
+
+                    {/* Account  */}
                     <div className="flex flex-col gap-4 pr-4">
-                        <h2 className="text-white text-center font-bold">Social Media</h2>
-                        <ul className="flex flex-row gap-4 text-sm">
-                            {Socials.map((social, index) => (
+                        <h2 className="text-white text-center font-bold">Account</h2>
+                        <ul className="flex flex-col gap-4 text-sm">
+                            {AccountItem.map((account, index) => (
                                 <li key={index}>
-                                    <Link target="_blank" href={social.href} className="text-white hover:text-[#FFD700] transition-all duration-300 flex items-center gap-2">
-                                        {social.icons} 
+                                    <Link href={account.href} className="text-white hover:text-[#FFD700] transition-all duration-300 flex items-center gap-2">
+                                        {account.name} 
                                     </Link>
                                 </li>
                             ))}
                         </ul>
                     </div>
+
+                    {/* Contact Us  */}
                     <div className="flex flex-col gap-4 pr-4">
                         <h2 className="text-white  font-bold">Contact Us</h2>
                         <ul className="flex flex-col gap-4 text-sm">
@@ -52,7 +59,22 @@ export default function Footer() {
                         </ul>
                     </div>
                 </div>
-                <div className="mt-4 border-t border-[#FFD700] pt-4">
+
+                {/* Social Media  */}
+                <div className="flex flex-col gap-4">
+                        <h2 className="text-white text-center font-bold">Follow Us</h2>
+                        <ul className="flex flex-row gap-4 text-sm justify-center">
+                            {Socials.map((social, index) => (
+                                <li key={index}>
+                                    <Link target="_blank" href={social.href} className="text-white hover:text-[#FFD700] transition-all duration-300 flex items-center gap-2">
+                                        {social.icons} 
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                </div>
+
+                <div className="mt-8 border-t border-[#FFD700] pt-4">
                     <p className="text-center text-sm">
                         &copy; {new Date().getFullYear()} Modern Fashion & Family. All rights reserved.
                     </p>

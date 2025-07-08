e9e4e21761a80ebf975f48fb3ebd780781ed7e57
Enoch <jesulanaenoch@gmail.com>
2025-07-08 12:47:11 +0100

    feat: enhance About page layout with new hero section
    - Added a new hero section to the About page, featuring an overlay and an image for improved visual appeal.
    - Updated the structure to include a title and description, emphasizing the brand's identity and mission.
    - Removed the previous HeroSection component to streamline the layout and enhance user engagement.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,25 +1,50 @@
 import Category from "../components/Homepage/Category";
 import { AboutImage } from "../components/models/ImagesList";
 import HeroSection from "../components/shared/HeroSection";
+import { sectionPadding } from "../styles/styles";
 import AboutOverview from "./AboutOverview";
 import CallToAction from "./CallToAction";
 import CoreValues from "./CoreValues";
 import OurPromise from "./OurPromise";
+import Image from "next/image";
+import images from "@/public/images";
 
 export default function AbouutPage() {
     return (
+        <>
+           <div className={` mb-10 relative`}>
+            {/* Overlay */}
+            <div className="absolute top-0 left-0 w-full h-full bg-black/60"></div>
+            {/* Image */}
+            <Image 
+            priority
+            quality={100}
+            src={images.AboutHero_Img1}  alt="About Us" className="w-full md:h-[585px] object-cover object-top" /> 
+            {/* {ImageCarousel.map((image, index) => (
+                <Image key={index} src={image.image} alt={image.alt} width={100} height={100} className="w-full md:h-[450px] object-cover object-[50%_10%]" />
+            ))} */}
+            <div className={` ${sectionPadding} text-white absolute top-0 left-0 w-full h-full flex flex-col gap-4 justify-center`}> 
+                    <h2 
+                    className="text-2xl md:text-6xl font-bold text-center md:text-left md:mt-0 mt-10">
+                        About Modern Haven
+                    </h2>
+                    <p 
+                    className="text-sm md:text-lg text-gray-200 w-full md:w-[55%] text-center md:text-left">
+                        <span className="text-2xl font-bold">
+                            Discover the Heart of Modern Haven
+                        </span><br />
+                        We’re more than just a store — we’re a lifestyle. Built for families who love fashion and tech, Modern Haven brings you style, comfort, and quality all in one place.
+                    </p>
+                    {/* Please fill out the form below and we'll get back to you as soon as possible. */}
+            </div>  
+        </div>
         <div>
-            <HeroSection 
-            plaintitle="About Modern Haven"
-            description="Fashion and tech for the modern family."
-            image={AboutImage}
-            />
             <AboutOverview />
             <CoreValues />
             <Category />
             <OurPromise />
             <CallToAction />
-
         </div>
+        </>
     )
 }

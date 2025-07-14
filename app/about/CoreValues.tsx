470d6d5431e940c93e92cae986546143fce51444
Enoch <jesulanaenoch@gmail.com>
2025-07-14 13:21:20 +0100

    refactor: update CoreValues component icons and styling
    - Replaced placeholder icons with specific icons for each core value to enhance visual representation.
    - Adjusted component layout by increasing height for better spacing and readability.
    - Modified text styling for improved consistency across the component.

--- app/about/CoreValues.tsx
+++ app/about/CoreValues.tsx
@@ -6,27 +6,27 @@ export default function CoreValues() {
         {
             name: "Integrity",
             description: "We are committed to honest, transparent service, offering products you can trust.",
-            Icon: <Icons.Phone />
+            Icon: <Icons.Badge className="w-10 h-6 text-[#FFD700]"/>
         },
         {
             name: "Excellence",
             description: "We deliver quality fashion and lifestyle essentials designed to elevate your everyday life.",
-            Icon: <Icons.Phone />
+            Icon: <Icons.Excellence className="w-10 h-6 text-[#FFD700]"/>
         },
         {
             name: "Customer-Centered",
             description: "Your style and comfort come first. Every product is thoughtfully chosen with your needs in mind..",
-            Icon: <Icons.Phone />
+            Icon: <Icons.Thumbs className="w-10 h-6 text-[#FFD700]"/>
         },
         {
             name: "Family-Focused Impact",
             description: "We aim to bring style and joy to families everywhere, creating moments of connection through fashion and gadgets.",
-            Icon: <Icons.Phone />
+            Icon: <Icons.Community className="w-10 h-6 text-[#FFD700]"/>
         },
         {
             name: "Innovation",
             description: "We stay ahead of trends and technology to bring you modern styles and gadgets that fit your lifestyle.",
-            Icon: <Icons.Phone />
+            Icon: <Icons.Innovayion className="w-10 h-6 text-[#FFD700]"/>
         },
     ]
     return (
@@ -39,14 +39,14 @@ export default function CoreValues() {
             </p>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4 text-white">  
                 {CoreValues.slice(0, 3).map((item, index) => (
-                    <div key={index} className="text-center w-full h-[200px] flex flex-col space-y-4 p-5 bg-[#0F2B22] shadow-[#0F2B22] hover:shadow-2xl rounded-md transition-all duration-300">
+                    <div key={index} className="text-center w-full h-[230px] flex flex-col justify-center space-y-4 p-5 bg-[#0F2B22] shadow-[#0F2B22] hover:shadow-2xl rounded-md transition-all duration-300">
                         <span className="mx-auto">
                             {item.Icon}
                         </span>
-                        <h2 className="md:text-2xl text-xl font-bold">
+                        <h2 className="text-xl font-bold">
                             {item.name}
                         </h2>
-                        <p className="md:text-sm text-gray-600">
+                        <p className="md:text-sm">
                             {item.description}
                         </p>
                     </div>
@@ -54,11 +54,11 @@ export default function CoreValues() {
             </div>
             <div className="flex flex-col md:flex-row gap-4 justify-center mt-4 text-white">
             {CoreValues.slice(3, 5).map((item, index) => (
-                    <div key={index} className="text-center w-full md:w-[350px] h-[200px] flex flex-col items-center space-y-4 p-5 bg-[#0F2B22] shadow-[#0F2B22] hover:shadow-2xl rounded-md transition-all duration-300">
+                    <div key={index} className="text-center w-full md:w-[350px] h-[230px] flex flex-col justify-center space-y-4 p-5 bg-[#0F2B22] shadow-[#0F2B22] hover:shadow-2xl rounded-md transition-all duration-300">
                         <span className="mx-auto">
                             {item.Icon}
                         </span>
-                        <h2 className=" font-bold">
+                        <h2 className="text-xl font-bold">
                             {item.name}
                         </h2>
                         <p className="md:text-sm">

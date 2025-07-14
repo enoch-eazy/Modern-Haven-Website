5645b8472fb9d9f4a5ab8859e7db0e1aa686b751
Enoch <jesulanaenoch@gmail.com>
2025-07-14 13:23:31 +0100

    feat: implement MenShop component with form for adding new shop items
    - Introduced MenShop component that includes a form for users to input shop details such as title, description, and price.
    - Implemented form validation to ensure all fields are filled before submission.
    - Integrated API call to create a new shop entry upon form submission, with navigation to the shop page on success.

--- app/shop/men/page.tsx
+++ app/shop/men/page.tsx
@@ -1,7 +1,82 @@
+"use client"
+
+import { sectionPadding } from "@/app/styles/styles";
+import Button from "@/app/ui/button";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+
 export default function MenShop() {
+    const router = useRouter();
+    const [formValue, setFormValue] = useState({
+        title: "",
+        description: "",
+        price: ""
+    });
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
+        if (name === "price") {
+            const numericValue =  value.replace(/\D/g, "")
+            setFormValue((prev) => ({...prev, [name]: numericValue}))
+        }
+        setFormValue((prev) => (
+                {...prev, [name]: value}
+            )
+        )
+    };
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        if (!formValue.title || !formValue.description || !formValue.price) {
+            alert("Title, Description, and Price are required.");
+            return;
+        }
+        try {
+            const res = await fetch("http://localhost:3000/api/shops/", {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json",
+                },
+                body: JSON.stringify({title: formValue.title, price: formValue.price, description: formValue.description})
+             } );
+             if (res.ok) {
+                router.push("/shop");
+             } else {
+                throw new Error("Failed to create Shop");
+             }
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return (
-        <section>
-            <h2>Welcome To this Shop</h2>
+        <section className={`${sectionPadding} mt-20 py-20 `}>
+            <form onSubmit={handleSubmit} className="w-1/2 flex flex-col gap-3 mt-10 mx-auto">
+                <input 
+                onChange={handleChange}
+                name="title"
+                value={formValue.title}
+                className="border border-slate-300 px-8 py-2"
+                type="text"
+                placeholder="Topic Title" />
+                <input 
+                onChange={handleChange}
+                name="description"
+                value={formValue.description}
+                className="border border-slate-300 px-8 py-2"
+                type="text"
+                placeholder="Your Description"
+                />
+                <input 
+                onChange={handleChange}
+                name="price"
+                value={formValue.price}
+                className="border border-slate-300 px-8 py-2"
+                type="text"
+                placeholder="Price"
+                />
+                <Button >
+                    Update Topic
+                </Button>
+            </form>
         </section>
     )
 }

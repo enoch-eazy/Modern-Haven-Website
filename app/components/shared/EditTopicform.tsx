0fdc6894af76d9f18c85b9de52fcd61a42e2d4d2
Enoch <jesulanaenoch@gmail.com>
2025-07-14 13:22:03 +0100

    feat: add EditTopicForm component for updating shop details
    - Introduced a new EditTopicForm component to facilitate editing of shop information including title, description, and price.
    - Implemented form handling with validation to ensure required fields are filled before submission.
    - Integrated API call to update shop details on the server upon form submission.

---
+++ app/components/shared/EditTopicform.tsx
@@ -0,0 +1,79 @@
+"use client"
+
+import { sectionPadding } from "@/app/styles/styles";
+import Button from "@/app/ui/button";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function EditShopForm ({id, title, description, price}: any ) {
+    const router = useRouter()
+    const [formValue, setFormValue] = useState({
+        newTitle: title,
+        newDescription: description,
+        newPrice: price
+    });
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
+        if (name === "price") {
+            const numericValue =  value.replace(/\D/g, "")
+            setFormValue((prev) => ({...prev, [name]: numericValue}))
+        }
+        setFormValue((prev) => (
+                {...prev, [name]: value}
+            )
+        )
+    };
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        try {
+            const res = await fetch(`http://localhost:3000/api/shops/${id}`, {
+                method: "PUT",
+                headers: {"Content-type": "application/json"},
+                body: JSON.stringify({newTitle: formValue.newTitle, newDescription:formValue.newDescription, newPrice: formValue.newPrice})
+            });
+            if (!res.ok) {
+                throw new Error("Failed to Update")
+            } 
+            router.refresh();
+            router.push("/shop");
+        } catch (error) {
+            console.log(error)
+        }
+        if (!formValue.newTitle || !formValue.newDescription || !formValue.newPrice) {
+            alert("Title, Description, and Price are required.");
+            return;
+        }
+    }
+    return (
+        <section className={`${sectionPadding} mt-20 py-20 `}>
+            <form onSubmit={handleSubmit} className="w-1/2 flex flex-col gap-3 mt-10 mx-auto">
+                <input 
+                onChange={handleChange}
+                name="newTitle"
+                value={formValue.newTitle}
+                className="border border-slate-300 px-8 py-2"
+                type="text"
+                placeholder="Topic Title" />
+                <input 
+                onChange={handleChange}
+                name="newDescription"
+                value={formValue.newDescription}
+                className="border border-slate-300 px-8 py-2"
+                type="text"
+                placeholder="Your Description"
+                />
+                <input 
+                onChange={handleChange}
+                name="newPrice"
+                value={formValue.newPrice}
+                className="border border-slate-300 px-8 py-2"
+                type="text"
+                placeholder="Price"
+                />
+                <Button >
+                    Update Topic
+                </Button>
+            </form>
+        </section>
+    )
+}

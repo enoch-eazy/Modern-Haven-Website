95b032e803ef9e53a51a380b33e680db31783896
Enoch <jesulanaenoch@gmail.com>
2025-07-25 17:31:55 +0100

    feat: add OurPromise component to communicate brand commitment and enhance About page content

---
+++ app/components/about/OurPromise.tsx
@@ -0,0 +1,17 @@
+import { sectionPadding } from "../../styles/styles";
+
+export default function OurPromise() {
+    return (
+        <section className={`${sectionPadding} py-0 md:py-20`}>
+            <div className="flex flex-col md:flex-row gap-10">
+                <h2 className="text-2xl font-bold">
+                    Our Promise
+                </h2>
+                <p className="text-gray-500">
+                    Modern Haven is a fashion and tech company that provides the best products for the modern family.
+                </p>
+            </div>
+
+        </section>
+    )
+}

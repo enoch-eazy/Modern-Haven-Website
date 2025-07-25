0fabf68804ce6ad70747e74ee70911c220989615
Enoch <jesulanaenoch@gmail.com>
2025-07-25 17:29:51 +0100

    feat: add CallToAction component to promote brand message on the About page

---
+++ app/components/about/CallToAction.tsx
@@ -0,0 +1,16 @@
+import { sectionPadding } from "../../styles/styles";
+
+export default function CallToAction() {
+    return (
+        <section className={`${sectionPadding} py-0 md:py-20`}>
+            <div className="flex flex-col md:flex-row gap-10">
+                <h2 className="text-2xl font-bold">
+                    Call to Action
+                </h2>
+                <p className="text-gray-500">
+                    Modern Haven is a fashion and tech company that provides the best products for the modern family.
+                </p>
+            </div>
+        </section>
+    )
+}

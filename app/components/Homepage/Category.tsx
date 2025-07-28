5eaae94b48932f94d648395fb05139605438dc08
Enoch <jesulanaenoch@gmail.com>
2025-07-28 10:07:37 +0100

    fix: remove unused framer-motion import in Category component

--- app/components/Homepage/Category.tsx
+++ app/components/Homepage/Category.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import CategorySection from "./CategorySection";
 import { CategoryItem } from "@/app/components/models/Category";
-import { easeInOut, motion } from "framer-motion";
+
 
 const Category: React.FC = () => {
     return (

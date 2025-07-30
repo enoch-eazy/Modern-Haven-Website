f22b4adc419e62b775e9068f3ca7a65b3ffe246a
Enoch <jesulanaenoch@gmail.com>
2025-07-30 12:03:23 +0100

    refactor: streamline LogOut component by extracting signOut logic into a separate function

--- app/components/shared/LogOut.tsx
+++ app/components/shared/LogOut.tsx
@@ -4,17 +4,18 @@ import { Icons } from "@/app/ui/icons";
 import { signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
-
-const router = useRouter();
-
 export default function LogOut() {
+    const router = useRouter();
+
+    const handleClick = async () => {
+        await signOut({ redirect: false }); // prevent next-auth from redirecting
+        router.push("/login");
+    };
+
     return (
         <button
-        onClick={() => {
-            signOut();
-            router.push("/login")
-        } }
-        className={`w-full md:w-64 justify-center flex items-center gap-2 bg-[#0F2B22] px-5 py-2 font-bold rounded-full hover:bg-[#FFD700] hover:text-red-600 text-red-400 transition-all duration-300`}
+            onClick={handleClick}
+            className={`w-full md:w-64 justify-center flex items-center gap-2 bg-[#0F2B22] px-5 py-2 font-bold rounded-full hover:bg-[#FFD700] hover:text-red-600 text-red-400 transition-all duration-300 cursor-pointer`}
         >
             LogOut <Icons.Logout className=""/>
         </button>

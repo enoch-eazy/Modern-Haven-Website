172f2071940f31bd93f8e632b220c675c79a650b
Enoch <jesulanaenoch@gmail.com>
2025-07-30 12:03:15 +0100

    style: add cursor pointer to Login button for better user experience

--- app/components/shared/Login.tsx
+++ app/components/shared/Login.tsx
@@ -8,7 +8,7 @@ export default function LogIn() {
     return (
         <button
         onClick={() => {router.push("/login")}}
-            className={`w-full md:w-64 justify-center flex items-center gap-2 bg-[#0F2B22] px-5 py-2 font-bold rounded-full hover:bg-[#FFD700] hover:text-[white] transition-all duration-300`}
+            className={`w-full md:w-64 justify-center flex items-center gap-2 bg-[#0F2B22] px-5 py-2 font-bold rounded-full hover:bg-[#FFD700] hover:text-[white] transition-all duration-300 cursor-pointer`}
         >
            Login <Icons.Login />
         </button>
